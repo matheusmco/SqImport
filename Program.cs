6c27346151060bdf4d179e10e12235d6112c6a10
Matheus Oliveira <matheus0mco@gmail.com>
2019-10-03 19:26:05 -0300

    feat:adding base files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
